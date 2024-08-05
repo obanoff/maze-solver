dd7ff43933749c3d4a7f64c9f1abb41df723031b
Yevhen Shliakhovskyi <obanoff@gmail.com>
2024-08-05 21:27:12 +0300

    Updated Maze class
    	- added __reset_cells_visited method that resets self._visited property of Cell objects in self.__cells of Maze to False

--- graphics/maze.py
+++ graphics/maze.py
@@ -35,6 +35,8 @@ class Maze:
 
         self.__break_walls_r(0, 0)
 
+        self.__reset_cells_visited()
+
     def __create_cells(self):
         # for testing purposes
         if type(self.__win) == Window:
@@ -139,6 +141,11 @@ class Maze:
             self.__animate()
 
             self.__break_walls_r(*move_to)
+
+    def __reset_cells_visited(self):
+        for column in self.__cells:
+            for cell in column:
+                cell._visited = False
 
     # for testing purposes
     @property
